ca072af028a0dec361e0450c3d0220ec0b3db506
Alan Vargas <alan.vargas@finerio.mx>
2019-07-31 22:14:50 -0500

    new xcode project

---
+++ FesANews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FesANews
+//
+//  Created by Alan Vargas on 31.07.2019.
+//  Copyright © 2019 Alan Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
